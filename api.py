7c19802dc44d4b66c495d4951e8786b4f36ebe56
Rattaphum <66010704@kmitl.ac.th>
2024-03-07 02:04:14 +0700

    view FlightInstances matches and view all flight seats

---
+++ api.py
@@ -0,0 +1,16 @@
+from main import *
+import uvicorn
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/flight_instance_matches", tags=["1: Get + Select Flight Instance"])
+def get_flight_instances_matches(froml : str, to : str, depart_date : str, return_date : Optional[str] = None):
+    return nokair.get_flight_instance_matches(froml, to, depart_date, return_date)
+
+@app.get("/get_all_seats", tags=["1: Get + Select Flight Instance"])
+def get_all_seats(flight_number : str, date : str):
+    return nokair.get_flight_instance(flight_number, date).flight_seat_list
+# if __name__ == "__main__":
+#     uvicorn.run("api:app", host="127.0.0.1", port=8000, log_level="info")
